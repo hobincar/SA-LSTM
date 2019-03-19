6fe23b2f5370c0a37936c97b152c59a65c252635
Hobin Ryu <hobincar@kaist.ac.kr>
2019-03-19 14:20:35 +0000

    Fix bugs when using bi-LSTM or multi-layer LSTM.

--- models/decoder.py
+++ models/decoder.py
@@ -23,7 +23,7 @@ class Decoder(nn.Module):
         self.embedding = nn.Embedding(self.output_size, self.embedding_size)
 
         self.attention = TemporalAttention(
-            hidden_size=self.num_layers * self.num_directions * self.hidden_size,
+            hidden_size=self.num_directions * self.hidden_size,
             feat_size=self.feat_size,
             bottleneck_size=self.attn_size)
 
@@ -35,20 +35,29 @@ class Decoder(nn.Module):
             dropout=self.rnn_dropout_p,
             bidirectional=True if self.num_directions == 2 else False)
 
-        self.out = nn.Linear(self.num_layers * self.num_directions * self.hidden_size, self.output_size)
+        self.out = nn.Linear(self.num_directions * self.hidden_size, self.output_size)
+
+    def get_last_hidden(self, hidden):
+        last_hidden = hidden[0] if isinstance(hidden, tuple) else hidden
+        last_hidden = last_hidden.view(self.num_layers, self.num_directions, last_hidden.size(1), last_hidden.size(2))
+        last_hidden = last_hidden.transpose(2, 1).contiguous()
+        last_hidden = last_hidden.view(self.num_layers, last_hidden.size(1), self.num_directions * last_hidden.size(3))
+        last_hidden = last_hidden[-1]
+        last_hidden = last_hidden.squeeze(0)
+        return last_hidden
 
     def forward(self, input, hidden, feats):
         embedded = self.embedding(input)
 
-        feats, attn_weights = self.attention(hidden, feats)
+        last_hidden = self.get_last_hidden(hidden)
+        feats, attn_weights = self.attention(last_hidden, feats)
 
         input_combined = torch.cat((
             embedded,
             feats.unsqueeze(0)), dim=2)
-        _, hidden = self.rnn(input_combined, hidden)
+        output, hidden = self.rnn(input_combined, hidden)
 
-        output = hidden[0] if self.rnn_type == 'LSTM' else hidden
-        output = torch.cat([ o for o in output ], dim=1)
+        output = output.squeeze(0)
         output = self.out(output)
         output = F.log_softmax(output, dim=1)
         return output, hidden, attn_weights
